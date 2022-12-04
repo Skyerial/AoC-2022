2e3cc66bf448aaca21c0e2985c5921dc4e5070cd
Skyerial <djoppenhuizen@hotmail.nl>
2022-12-04 14:41:46 +0100

    add day 4 of AoC

---
+++ day4/main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+// #include <cstring>
+#include <string_view>
+// includes for the timing
+#include <chrono>
+
+int test(std::string elf1, std::string elf2) {
+    std::string delim = "-";
+    bool elf1_smaller = false;
+    bool elf1_bigger = false;
+    bool elf2_smaller = false;
+    bool elf2_bigger = false;
+
+    if (std::stoi(elf2.substr(0, elf2.find(delim))) < 
+        std::stoi(elf1.substr(0, elf1.find(delim)))) {
+        elf1_smaller = false;
+        elf2_smaller = true;
+    } else if (std::stoi(elf2.substr(0, elf2.length())) == 
+        std::stoi(elf1.substr(0, elf1.length()))) {
+        elf1_smaller = true;
+        elf2_smaller = true;
+    } else {
+        elf1_smaller = true;
+        elf2_smaller = false;
+    }
+    if (std::stoi(elf2.substr(elf2.find(delim) + 1, elf2.length())) > 
+        std::stoi(elf1.substr(elf1.find(delim) + 1, elf1.length()))) {
+        elf1_bigger = false;
+        elf2_bigger = true;
+    } else if (std::stoi(elf2.substr(elf2.find(delim) + 1, elf2.length())) == 
+        std::stoi(elf1.substr(elf1.find(delim) + 1, elf1.length()))) {
+        elf1_bigger = true;
+        elf2_bigger = true;
+    } else {
+        elf1_bigger = true;
+        elf2_bigger = false;
+    }
+
+    if ((elf1_bigger && elf1_smaller) || (elf2_bigger && elf2_smaller)) {
+        return 1;
+    }
+    return 0;
+}
+
+int any_overlap(std::string elf1, std::string elf2) {
+    std::string delim = "-";
+
+    if (std::stoi(elf2.substr(0, elf2.find(delim))) <= 
+        std::stoi(elf1.substr(elf1.find(delim) + 1, elf1.length())) &&
+        std::stoi(elf2.substr(elf2.find(delim) + 1, elf2.length())) >= 
+        std::stoi(elf1.substr(0, elf1.find(delim)))) {
+        printf("1\n");
+        return 1;
+    }
+
+    printf("0\n");
+    return 0;
+}
+
+void read_input(std::string file, int (*function)(std::string, std::string)) {
+    std::ifstream myfile(file);
+    std::string myline;
+    int result = 0;
+    std::string elf1, elf2;
+    std::string delim = ",";
+    std::string test_string = "hello man";
+
+    if (myfile.is_open()) {
+        while(std::getline(myfile, myline)) {
+            elf1 = myline.substr(0, myline.find(delim));
+            elf2 = myline.substr(myline.find(delim) + 1, myline.length());
+            printf("%s\t", myline.data());
+            result += (*function)(elf1, elf2);
+        }
+    } else {
+        perror("Can't open file");
+        return;
+    }
+
+    printf("Amount of assignments with full overlap: %d\n", result);
+}
+
+int main(int argc, char *argv[]) {
+    auto start = std::chrono::high_resolution_clock::now();
+    if ((std::string)argv[1] == "test") {
+        read_input("test.txt", &any_overlap);
+    }
+    if ((std::string)argv[1] == "p1") {
+        read_input("input.txt", &test);
+    }
+    if ((std::string)argv[1] == "p2") {
+        read_input("input.txt", &any_overlap);
+    }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>
+                                                                (stop - start);
+
+    std::cout << "Done in: " << duration.count() << " ms" << std::endl;
+    return 0;
+}
